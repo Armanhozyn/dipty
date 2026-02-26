7cede5064d339249e3d23f006dd745bd20aeb8e1
Armanhozyn <armanhossen591@gmail.com>
2026-02-27 03:58:48 +0600

    Done

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dipty');
 });
+
+Route::get('/optimize-clear', function () {
+    Artisan::call('optimize:clear');
+
+    return 'Application cache cleared successfully.';
+});
+
 Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
